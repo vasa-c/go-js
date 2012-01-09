5ff5951c3ea1fe6e4c998c7e530cee9c3bc6fcd9
Grigoriev Oleg <go.vasac@gmail.com>
2012-01-09 21:44:08 +0400

    basic structure of sources and unit-tests

---
+++ src/go.js
@@ -0,0 +1,22 @@
+/**
+ * go.js: библиотека для упрощения некоторых вещей в JavaScript
+ *
+ * В данном файле определяется только ядро библиотеки и модуль go.Lang.
+ * Остальные модули располагаются в других файлах.
+ *
+ * @package go.js
+ * @author  Григорьев Олег aka vasa_c (http://blgo.ru/)
+ * @version 1.0-beta
+ * @license MIT (http://www.opensource.org/licenses/mit-license.php)
+ * @link    https://github.com/vasa-c/go-js
+ */
+"use strict";
+
+var go = (function () {
+
+	var go = {
+		'VERSION' : "1.0-beta"
+	};
+
+	return go;
+}());
