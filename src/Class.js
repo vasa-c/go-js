47cb52b1636a2c6c6326decebef020d8d2a8543b
Grigoriev Oleg <go.vasac@gmail.com>
2012-01-14 18:03:47 +0400

    Class: [ref]

--- src/Class.js
+++ src/Class.js
@@ -13,25 +13,194 @@ if (!window.go) {
     throw new Error("go.core is not found");
 }
 
-go("Class", (function () {
+go("Class", (function (go) {
 
-    function Class(props) {
+    var
+        Class,
+        RootSettings,
+        RootPrototype,
+        ClassCreatorPrototype,
+        ClassCreatorConstructor;
 
-        function FakeConstruct() {
+    /**
+     * Настройки корневого класса
+     */
+    RootSettings = {
+        'names': {
+            'constructor ': "__construct",
+            'destructor'  : "__destruct",
+            'destroy'     : "destroy",
+            'instance_of' : "instance_of"
         }
-        function Construct() {
-            if (!(this instanceof Construct)) {
-                // @todo когда this может быть внезапно instanceof?
-                var obj = new FakeConstruct();
-                Construct.apply(obj, arguments);
-                return obj;
+    };
+
+    /**
+     * Прототип корневого класса
+     * Свойства и методы, доступные во всех объектах
+     */
+    RootPrototype = {
+        'go$type'   : "go.object",
+        '$settings' : RootSettings,
+        'toString': function() {
+            return "[go.object]";
+        }
+    };
+    RootPrototype[RootSettings.names.constructor] = function () {};
+    RootPrototype[RootSettings.names.destructor] = function () {};
+    RootPrototype[RootSettings.names.destroy] = function () {
+        this.__destruct();
+    };
+    RootPrototype[RootSettings.names.instance_of] = function(C) {
+        return (this instanceof C);
+    };
+
+    /**
+     * Прототип объектов, создающих конструкторы новых классов
+     *
+     * @var function Class
+     *      итоговая функция-конструктор
+     * @var hash proto
+     *      объект, служащий в качестве прототипа для класса
+     * @var hash props
+     *      переданные в класс поля
+     * @var Class parent
+     *      основной предок класса
+     * @var Class[] otherParents
+     *      список дополнительных предков
+     * @var hash settings
+     *      настройки класса
+     */
+    ClassCreatorPrototype = {
+
+        /**
+         * Конструктор
+         *
+         * @param mixed parents
+         *        класс-предок или список предков (или null)
+         * @param hash props
+         *        набор свойств и методов класса
+         */
+        '__construct': function(parents, props) {
+            if (!props) {
+                props = parents;
+                parents = null;
             }
-        }
-        Construct.prototype = props;
-        FakeConstruct.prototype = props;
+            this.props = props;
+            this.separateParents(parents);
+        },
 
-        return Construct;
-    }
+        /**
+         * Деструктор
+         */
+        '__destruct': function() {
+        },
+
+        /**
+         * Создание класса
+         */
+        'create': function() {
+            this.createBlankPrototype();
+            this.loadSettings();
+            this.createClass();
+            this.fillClassProperties();
+        },
+
+        /**
+         * Получение класса
+         */
+        'getClass': function () {
+            return this.Class;
+        },
+
+        /**
+         * Разделение предков на основные и второстепенные
+         *
+         * @param mixed parents
+         */
+        'separateParents': function (parents) {
+            if (!parents) {
+                this.parent = null;
+                this.otherParents = [];
+            } else if (typeof parents === "function") {
+                this.parent = parents;
+                this.otherParents = [];
+            } else {
+                this.parent = parents[0];
+                this.otherParents = parents.slice(1);
+            }
+            if ((!this.parent) && Class.Root) {
+                this.parent = Class.Root;
+            }
+        },
+
+        /**
+         * Создание заготовки прототипа
+         */
+        'createBlankPrototype': function () {
+            if (this.parent) {
+                this.proto = new this.parent.Fake();
+            } else {
+                this.proto = {};
+            }
+            go.Lang.extend(this.proto, this.props);
+        },
+
+        /**
+         * Загрузка настроек класса
+         */
+        'loadSettings': function () {
+            if (this.parent) {
+                this.settings = this.parent.$settings;
+            } else {
+                this.settings = {};
+            }
+            if (this.props.$settings) {
+                go.Lang.extend(this.settings, this.props.$settings);
+                delete this.proto['$settings'];
+            }
+        },
+
+        /**
+         * Создание функции-конструктора
+         */
+        'createClass': function () {
+            this.Class = function C() {
+                if (!(this instanceof C)) {
+                    var instance = new C.Fake();
+                    C.apply(instance, arguments);
+                    return instance;
+                }
+            };
+            this.Class.prototype = this.proto;
+        },
+
+        /**
+         * Заполнение объекта класса нужными свойствами
+         */
+        'fillClassProperties': function() {
+            this.Class.Fake = function() {};
+            this.Class.Fake.prototype = this.proto;
+        },
+
+        'eoc': null
+    };
+    ClassCreatorConstructor = function (parents, props) {
+        this.__construct(parents, props);
+    };
+    ClassCreatorConstructor.prototype = ClassCreatorPrototype;
+
+    /**
+     * Функция создания нового класса
+     * @alias go.Class
+     */
+    Class = function (parents, props) {
+        var creator = new ClassCreatorConstructor(parents, props);
+        creator.create();
+        var C = creator.getClass();
+        creator.__destruct();
+        return C;
+    };
+    Class.Root = Class(null, RootPrototype);
 
     return Class;
-}()));
+}(go)));
