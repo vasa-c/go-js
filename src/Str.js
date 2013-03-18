7838e4318ece9cf88e99dc59a14961231b37147c
Grigoriev Oleg <go.vasac@gmail.com>
2013-03-18 13:44:14 +0400

    Str: trim, trimLeft, trimRight

--- src/Str.js
+++ src/Str.js
@@ -12,12 +12,73 @@ if (!window.go) {
     throw new Error("go.core is not found");
 }
 
-go("Str", function (go, global, undefined) {
+/**
+ * @namespace go.Str
+ */
+go("Str", function () {
+    "use strict";
 
-    return {
+    var Str = {},
+        nativeProto = String.prototype,
+        nativeTrim = nativeProto.trim,
+        nativeTrimLeft = nativeProto.trimLeft,
+        nativeTrimRight = nativeProto.trimRight;
+
+    /**
+     * Обрезка крайних пробелов
+     *
+     * @name go.Str.trim
+     * @public
+     * @param {String} s
+     * @return {String}
+     */
+    if (nativeTrim) {
+        Str.trim = function trim(s) {
+            return nativeTrim.call(s);
+        };
+    } else {
+        Str.trim = function (s) {
+            return Str.trimLeft(Str.trimRight(s));
+        };
+    }
 
 
+    /**
+     * Обрезка левых пробелов
+     *
+     * @name go.Str.trimLeft
+     * @public
+     * @param {String} s
+     * @return {String}
+     */
+    if (nativeTrimLeft) {
+        Str.trimLeft = function trimLeft(s) {
+            return nativeTrimLeft.call(s);
+        };
+    } else {
+        Str.trimLeft = function (s) {
+            return s.replace(/^\s+/, "");
+        };
+    }
+
+    /**
+     * Обрезка правых пробелов
+     *
+     * @name go.Str.trim
+     * @public
+     * @param {String} s
+     * @return {String}
+     */
+    if (nativeTrimRight) {
+        Str.trimRight = function trimRight(s) {
+            return nativeTrimRight.call(s);
+        };
+    } else {
+        Str.trimRight = function (s) {
+            return s.replace(/\s+$/, "");
+        };
+    }
 
 
-    };
+    return Str;
 });
--- tests/Str.js
+++ tests/Str.js
@@ -11,6 +11,24 @@
 
 tests.module("Str");
 
-tests.test("", function () {
-    ok(true);
+tests.test("trim, ltrim, rtrim", function () {
+    var s1 = "one two three",
+        s2 = "русский",
+        s3 = "  \t  one two  \t\n",
+        s4 = "   рус   ";
+    equal(go.Str.trim(s1), "one two three");
+    equal(go.Str.trimLeft(s1), "one two three");
+    equal(go.Str.trimRight(s1), "one two three");
+
+    equal(go.Str.trim(s2), "русский");
+    equal(go.Str.trimLeft(s2), "русский");
+    equal(go.Str.trimRight(s2), "русский");
+
+    equal(go.Str.trim(s3), "one two");
+    equal(go.Str.trimLeft(s3), "one two  \t\n");
+    equal(go.Str.trimRight(s3), "  \t  one two");
+
+    equal(go.Str.trim(s4), "рус");
+    equal(go.Str.trimLeft(s4), "рус   ");
+    equal(go.Str.trimRight(s4), "   рус");
 });
