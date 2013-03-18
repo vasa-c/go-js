98003f095bc326c711bfea39630f1b3af4b40d22
Grigoriev Oleg <go.vasac@gmail.com>
2013-03-18 13:28:21 +0400

    Str: go.Str module

---
+++ src/Str.js
@@ -0,0 +1,23 @@
+/**
+ * go.Str: строковые функции
+ *
+ * @package    go.js
+ * @subpackage Str
+ * @author     Григорьев Олег aka vasa_c (http://blgo.ru/)
+ */
+/*jslint nomen: true, es5: true */
+/*global go, window */
+
+if (!window.go) {
+    throw new Error("go.core is not found");
+}
+
+go("Str", function (go, global, undefined) {
+
+    return {
+
+
+
+
+    };
+});
---
+++ tests/Str.js
@@ -0,0 +1,16 @@
+/**
+ * Тестирование модуля go.Str
+ *
+ * @package    go.js
+ * @subpackage Str
+ * @author     Григорьев Олег aka vasa_c (http://blgo.ru/)
+ */
+/*jslint node: true, nomen: true */
+/*global go, tests, ok, equal, deepEqual, throws, $ */
+"use strict";
+
+tests.module("Str");
+
+tests.test("", function () {
+    ok(true);
+});
