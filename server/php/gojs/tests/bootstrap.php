51e74bcf12139bd045a225cd90fedf0c67252ec9
Grigoriev Oleg <go.vasac@gmail.com>
2013-04-11 15:18:53 +0400

    server (php.gojs): phpunit bootstrap

---
+++ server/php/gojs/tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Инициализация юнит-тестов
+ *
+ * @package go\gojs
+ * @author  Григорьев Олег aka vasa_c <go.vasac@gmail.com>
+ */
+
+require_once(__DIR__.'/../src/Autoloader.php');
+\go\gojs\Autoloader::register();
